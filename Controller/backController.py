834652765de00280cae77ebafe6b6ef345fabf47
brandon100477 <104177639+brandon100477@users.noreply.github.com>
2024-01-18 09:34:19 -0500

    estilos de segunda ventana (falta funciones)

--- Controller/backController.py
+++ Controller/backController.py
@@ -3,28 +3,38 @@ from Views.sesion import inicio
 from Views.view1 import welcome
 
 
-global blanco, negro, gris, azul, log
+global blanco, negro, gris, azul, rojo, azul_fg, rojo_fg, gris_claro
 blanco = '#FFFFFF'
 gris = '#CFCFCF'
 azul = '#3A94E7'
-
+azul_fg = '#004B90'
+rojo = '#E73A3A'
+rojo_fg = '#DE0000'
+negro = '#000000'
+gris_claro = '#FBFBFB'
 #función de prueba
 def fnSuma(n1, n2):
     print(int(n1) + int(n2))
 #Funcion para estilos de Label
 def create_label(text, window):
     return Label(window, text=text, font=("Arial", 14), bg=blanco)
+
+def create_label2(text, window):
+    return Label(window, text=text, font=("Arial", 28), bg=azul, fg=blanco)
 #Función de estilos de botón
 def create_Button(text, window, command):
-    return Button(window, text=text, font=("Arial", 16),command=command, background="#3A94E7")
+    return Button(window, text=text, font=("Arial", 16),command=command, bg=azul, fg=blanco)
+
+def btn_inicio(text, window, command):
+    return Button(window, text=text, font=("Arial", 16),command=command, bg=azul, fg=blanco)
 
 def close_Button(text, window, command):
-    return Button(window, text=text, font=("Arial", 16),command=command, background="#E73A3A")
+    return Button(window, text=text, font=("Arial", 16),command=command, bg=rojo, fg= negro)
 
 def open1(self):
     self.window.destroy()
     welcome()
     
 def open2(self):
-    self.window2.destroy()
+    self.window.destroy()
     inicio()
--- Views/view1.py
+++ Views/view1.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label,Button, Entry, PhotoImage
+from tkinter import Tk, Label,Button, Entry, PhotoImage, Frame, Canvas
 import Controller.backController as backController
 from PIL import Image, ImageTk  #Instalar la libreria de PIL
 from customtkinter import CTk, CTkFrame #Instalar la libreria de customtkinter
@@ -7,16 +7,45 @@ from customtkinter import CTk, CTkFrame #Instalar la libreria de customtkinter
 class welcome():
     
     def __init__(self):
-        self.window2 = Tk()
-        self.window2.geometry("1366x768+105+20") # Creación de ventana
-        self.window2.title("Bienvenid@") #titulo
-        self.window2.configure(bg=backController.blanco) #Color de fondo de fondo
-        lbl = Label(self.window2, text = 'Bienvenido',font=("Arial", 28), bg=backController.blanco) 
-        lbl.place(relx=0.41, y=22, relheight=0.05, relwidth=0.19) #Posición del titulo de entrada
-        btn = backController.close_Button('Cerrar sesión', self.window2, command=lambda: backController.open2(self))
-        btn.place(relx=0.22, rely=0.89, relheight=0.06, relwidth=0.12)
-        #Creacion del logo
+        self.window = Tk()
+        self.window.geometry("1366x768+105+20") # Creación de ventana
+        self.window.title("Bienvenid@") #titulo
+        self.window.configure(bg=backController.blanco) #Color de fondo de fondo
         
+        header = Frame(self.window, bg=backController.azul, height=90)
+        header.pack(fill="x")
+
+                # Parte principal de la ventana dividida en izquierda y derecha
+        main_frame = Frame(self.window, bg= backController.blanco)
+        main_frame.pack(fill="both", expand=True)
+
+        left_half = Frame(main_frame, bg=backController.azul)  
+        left_half.place(relx=0.0, rely=0.0, relwidth=0.3, relheight=1)# width para en ancho y height para el alto del campo en azul
+        
+        lbl = backController.create_label2('Bienvenido', self.window)#Titulo y estilos
+        lbl.place(relx=0.41, y=22, relheight=0.05, relwidth=0.19) #Posición del titulo de entrada
+        
+        
+        search = Entry(self.window,bg=backController.gris_claro)
+        search.place(relx=0.35, rely=0.16, relheight=0.05, relwidth=0.6)
+        
+        
+        btn1 = backController.btn_inicio('Arrendatario', self.window, command=lambda: print("Arrendamiento"))
+        btn1.place(relx=0.05, rely=0.2, relheight=0.06, relwidth=0.18)
+        
+        btn2 = backController.btn_inicio('Agregar', self.window, command=lambda: print("Agregar"))
+        btn2.place(relx=0.05, rely=0.35, relheight=0.06, relwidth=0.18)
+        
+        btn3 = backController.btn_inicio('Ult. vez ingresado', self.window, command=lambda: print("Ult. vez ingresado"))
+        btn3.place(relx=0.05, rely=0.5, relheight=0.06, relwidth=0.18)
+        
+        btn4 = backController.btn_inicio('Filtrar', self.window, command=lambda: print("Filtrar"))
+        btn4.place(relx=0.05, rely=0.65, relheight=0.06, relwidth=0.18)
+        
+        
+        btn = backController.close_Button('Cerrar sesión', self.window, command=lambda: backController.open2(self))
+        btn.place(relx=0.05, rely=0.8, relheight=0.06, relwidth=0.18) #Botón para cerrar sesión
+        #Creacion del logo
         logo = PhotoImage(file= 'img/icon.png')
-        self.window2.call('wm', 'iconphoto', self.window2, logo )
+        self.window.call('wm', 'iconphoto', self.window, logo)
 
